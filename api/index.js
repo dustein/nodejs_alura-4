22c87a8728cd7fd035d522aee782bfd5c3d88161
dustein <steindu@gmail.com>
2022-04-05 11:53:09 -0300

    configrando mysql

--- api/index.js
+++ api/index.js
@@ -1,2 +1,14 @@
 import express from 'express';
+import bodyParser from 'body-parser';
 
+const app = express();
+
+app.use(bodyParser.json());
+
+const port = 3000;
+
+app.get("/teste", (req, res) => {
+    res.send({messagem: "OK funciona certo"});
+})
+
+app.listen(port, () => console.log("OnLINE !"));
